71a74f99b6c0a19773424f75c5eaa0d05ee2792e
Jed556 <jguiriba11@gmail.com>
2022-07-04 01:29:59 +0800

    Add remote restart command

---
+++ commands/Bot/restart.js
@@ -0,0 +1,75 @@
+const { MessageEmbed } = require('discord.js');
+const emb = require('../../config/embed.json');
+var Heroku = require('heroku-client');
+let os = require('os');
+
+// Variable checks (Use .env if present)
+require('dotenv').config();
+let OwnerID, Token, AppName, DynoName;
+if (process.env.ownerID && process.env.herokuToken && process.env.herokuApp && process.env.herokuDyno) {
+    OwnerID = process.env.ownerID;
+    Token = process.env.herokuToken;
+    AppName = process.env.herokuApp;
+    DynoName = process.env.herokuDyno;
+} else {
+    const { ownerID } = require('../../config/client.json');
+    const { herokuToken, herokuApp, herokuDyno } = require('../../config/heroku.json');
+    OwnerID = ownerID;
+    Token = herokuToken;
+    AppName = herokuApp;
+    DynoName = herokuDyno;
+}
+
+module.exports = {
+    name: "restart",
+    description: "Perform Apollø global restart",
+    help: "/restart (countdown)",
+    cooldown: 1000,
+    permissions: [],
+    allowedUIDs: [OwnerID],
+    options: [
+        {
+            name: "countdown",
+            description: "Countdown before restarting (seconds)",
+            type: 4,
+            required: false,
+        }
+    ],
+
+    run: async (client, interaction) => {
+        try {
+            const cd = interaction.options.getInteger("countdown");
+            if (os.hostname().length == 36)
+                interaction.reply({
+                    embeds: [new MessageEmbed()
+                        .setTimestamp()
+                        .setColor(emb.color)
+                        .setAuthor({ name: "HEROKU | restart.js", iconURL: emb.heroku })
+                        .setDescription(`**Restarting ${client.user.username}${cd ? ` in ${cd} sec${cd != 1 ? "s" : ""}` : "..."}**`)
+                        .setFooter({ text: client.user.username, iconURL: client.user.displayAvatarURL() })
+                    ],
+                    ephemeral: true
+                });
+            if (cd)
+                setTimeout(async () => {
+                    try {
+                        var heroku = new Heroku({ token: Token });
+                        heroku.delete('/apps/' + AppName + '/dynos/' + DynoName)
+                            .then(x => console.log(x));
+                    } catch { }
+                }, cd * 1000);
+            else interaction.reply({
+                embeds: [new MessageEmbed()
+                    .setTimestamp()
+                    .setColor(emb.errColor)
+                    .setAuthor({ name: "HEROKU | restart.js", iconURL: emb.heroku })
+                    .setDescription(`**${client.user.username} is currently not hosted by Heroku**`)
+                    .setFooter({ text: client.user.username, iconURL: client.user.displayAvatarURL() })
+                ],
+                ephemeral: true
+            });
+        } catch (e) {
+            console.log(String(e.stack));
+        }
+    }
+}
